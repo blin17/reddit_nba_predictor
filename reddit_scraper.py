1e7422c4255f93337ccd4de5178fe87996dfec54
Brian Lin <blin17@gmail.com>
2016-08-08 03:04:00 -0400

    scraper v2

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,22 +1,14 @@
-import praw
-import datetime
-import pprint
-
-
-# User Agent allows the reddit API to identify the script
-# <platform>:<app ID>:<version string> (by /u/<reddit username>)
-user_agent = "python:r_nba_app:v1 (by /u/fugitivedenim)"
-
-r = praw.Reddit(user_agent=user_agent)
-posts = r.get_subreddit('nba').get_hot(limit = 10)
-
 '''
+Reddit /r/nba Scraper - Scrapes reddit posts
+
+# Data Schema for the Post Table
 Post Table
 	----observed attributes---
 	id PRIMARY KEY INTEGER 
 	title TEXT
 	url	TEXT
 	url_domain TEXT
+	subreddit TEXT
 	permalink TEXT
 	score INTEGER 
 	num_comments INTEGER
@@ -27,38 +19,58 @@ Post Table
 	gilded INTEGER
 	is_self INTEGER // BOOLEAN
 	over_18 INTEGER // BOOLEAN
-
-
-	post_object = {
-		'title' : '',
-		'url' : '',
-		'url_domain' : '',
-		'permalink' : '',
-		'score' : 0,
-		'num_comments' : 0,
-		'num_duplicates' : 0,
-		'time_created' : 0,
-		'author_flair_text' : '',
-		'author' : '',
-		'gilded' : 0,
-		'is_self' : 0,
-		'over_18' : 0
-	}
 '''
+
+import praw
+import datetime
+import csv
+import json
+import pprint
+
+post_limit = 10
+clear_cache_limit = 1
+
+# User Agent allows the reddit API to identify the script
+# Format: <platform>:<app ID>:<version string> (by /u/<reddit username>)
+user_agent = "python:r_nba_app:v1 (by /u/fugitivedenim)"
+
+r = praw.Reddit(user_agent=user_agent)
+posts = r.get_subreddit('nba').get_hot(limit = post_limit)
 
 
 id = 0
 post_table = []
 
-#<class 'praw.objects.Submission'>
-for post in posts:
+
+csv = csv.writer(open("posts_data.csv", "wb+"))
+columns = ['id','title', 'url', 'url_domain', 'permalink', 'subreddit','score', 'num_comments', 
+		'num_duplicates', 'gilded', 'time_created', 'author_flair_text', 'author',
+		'is_self', 'over_18']
+csv.writerow(columns)
+
+def clear_posts():
+	global post_table
+	for p in post_table:
+		try:
+			csv.writerow([p['id'],p['title'].encode('utf8'), p['url'].encode('utf8'), p['url_domain'].encode('utf8'), p['permalink'].encode('utf8'), p['subreddit'], p['score'], p['num_comments'], p['num_duplicates'], p['gilded'], p['time_created'], p['author_flair_text'], p['author'], p['is_self'], p['over_18']])
+			for c in columns:
+
+		except Exception as e:
+			# Used to catch bad characters that break the csvwrite
+			print e
+			pass
+	post_table = []
+
+for post in posts: 
 	id += 1
+	print str(id) + " " + post.title
 	post_object = {
 		'id': id,
 		'title' : post.title,
 		'url' : post.url,
 		'url_domain' : post.domain,
 		'permalink' : post.permalink,
+		'subreddit' : post.subreddit,
 		'score' : post.score,
 		'num_comments' : post.num_comments,
 		'num_duplicates' : 0,
@@ -69,6 +81,7 @@ for post in posts:
 		'is_self' : 1 if post.is_self == 'True' else 0,
 		'over_18' : 1 if post.over_18 == 'True' else 0
 	}
+	# Gets all duplicates of the same link (most are in other subreddits)
 	duplicates = post.get_duplicates()
 	num_dupes = 0
 	dupes = []
@@ -81,6 +94,7 @@ for post in posts:
 			'url' : dupe.url,
 			'url_domain' : dupe.domain,
 			'permalink' : dupe.permalink,
+			'subreddit' : dupe.subreddit,
 			'score' : dupe.score,
 			'num_comments' : dupe.num_comments,
 			'num_duplicates' : 0,
@@ -99,8 +113,6 @@ for post in posts:
 	for item in dupes:
 		item['num_duplicates'] = num_dupes
 		post_table.append(item)
-
-pprint.pprint(post_table)
-
-
-#pprint.pprint(karma_by_subreddit)
+	if id % clear_cache_limit == 0:
+		clear_posts()
+clear_posts()
---
+++ scraper.py
@@ -0,0 +1,51 @@
+import praw
+import csv
+
+# Script Parameters
+post_limit = 1000 	# how many posts we want to scrape
+cache_limit = 50 	# after how many posts do we save cache to csv
+file_name = "post_data.csv" # output file name
+
+# User Agent allows the reddit API to identify the script
+# Format: <platform>:<app ID>:<version string> (by /u/<reddit username>)
+user_agent = "python:r_nba_app:v1 (by /u/fugitivedenim)"
+
+r = praw.Reddit(user_agent=user_agent)
+posts = r.get_subreddit('nba').get_hot(limit = post_limit)
+
+post_cache = []
+columns = ['id','title', 'url', 'domain', 'permalink', 'subreddit','score', 'num_comments', 
+		'num_duplicates', 'gilded', 'created', 'author_flair_text', 'author',
+		'is_self', 'over_18']
+
+csv_file = open(file_name,"wb+")
+writer = csv.writer(csv_file)
+writer.writerow(columns)
+
+def clear_cache():
+	for p in post_cache:
+		try:
+			writer.writerow([getattr(p[0],c) if c != 'num_duplicates' else p[1] for c in columns])
+		except Exception as e:
+			print e
+			print [getattr(p[0],c) if c != 'num_duplicates' else p[1] for c in columns]
+			pass
+
+counter = 1
+for post in posts:
+	print "{0}/{1} Posts".format(counter,post_limit)
+	counter +=1
+	#num_dupes
+	duplicates = post.get_duplicates()
+	num_dupes = 0
+	d_cache = []
+	for d in duplicates:
+		d_cache.append(d)
+		num_dupes +=1
+	post_cache.append((post,num_dupes))
+	post_cache.extend([(d,num_dupes)for d in d_cache])
+	if len(post_cache) > cache_limit:
+		clear_cache()
+		post_cache = []
+
+clear_cache()
